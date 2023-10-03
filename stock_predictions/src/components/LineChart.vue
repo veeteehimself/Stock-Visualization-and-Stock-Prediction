2dd1f61fc95eb6d5e1c235ca5e6523b47a86f0f3
jacky <jacky.nhiayi@gmail.com>
2023-10-03 13:53:20 -0700

    now gets API response from alphavantage

--- stock_predictions/src/components/LineChart.vue
+++ stock_predictions/src/components/LineChart.vue
@@ -10,20 +10,52 @@
 <script>
 
 import Chart from 'chart.js/auto';
+import {ref,computed} from "vue";
+import axios from "axios"
 
+
+// var request = require('request');
+
+// // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+// var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=EDOF97Z0B2DGU669';
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is successfully parsed as a JSON object:
+//       console.log(data);
+//     }
+// });
+
+let stockMarketHistory = []
+let stockMarketHistoryDates= []
+let stockMarketHistoryEpochDates = []
+let stockMarketHistoryPrices = []
+stockMarketHistory
+stockMarketHistoryDates
+stockMarketHistoryEpochDates
+stockMarketHistoryPrices
 let myChart
 myChart
 
 export default {
     name:"LineChart",
     mounted(){
+        this.createStockPriceHistoryResponse()
         this.updateStockPriceHistoryChart()
     },
     setup(){
         let updateStockPriceHistoryChart=() =>{
             const ctx = document.getElementById("myChart");
 
-            const labels = ["Jan","Feb", "Mar"]
+            const labels = ["Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
             const data = {
                 labels:labels,
                 datasets: [{
@@ -44,8 +76,22 @@ export default {
                 data: data,
             })
         }
+        let stockSymbol = ref("IBM")
+        let AlphaVantageApi_URL_LINK= computed(() =>{
+            return'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol='+stockSymbol.value+'&apikey=EDOF97Z0B2DGU669'
+        });
+        let createStockPriceHistoryResponse = (dateRange) =>{
+            axios.get(AlphaVantageApi_URL_LINK.value)
+            .then(response =>{
+                stockMarketHistory = response
+                console.log(stockMarketHistory)
+                dateRange;
+            })
+        }
+        console.log(AlphaVantageApi_URL_LINK.value)
         return {
-            updateStockPriceHistoryChart
+            updateStockPriceHistoryChart,
+            createStockPriceHistoryResponse
         }
     }
 }
